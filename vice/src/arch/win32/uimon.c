jtgans/vice-hacking

commit c1d6af3c9c97c1a2174fcf9b9b9cee348db22e60

Fixed to let winvice run on win95 again.
git-svn-id: svn+ssh://svn.code.sf.net/p/vice-emu/code/trunk@22332 379a1393-f5fb-40a0-bcee-ef074d9b53f7

vice/src/arch/win32/uimon.c
@@ -163,6 +163,40 @@ static HWND hwndActive = NULL;
 
 static void update_shown(void);
 
+#if defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__))
+void *uimon_icep(void **dest, void *xchg, void *compare)
+{
+    void *ret;
+#ifdef __x86_64__
+    __asm__ volatile ("lock; cmpxchgq %2,(%1)"
+        : "=a" (ret) : "r" (dest), "r" (xchg), "" (compare) : "memory");
+#else
+    __asm__ volatile ("lock; cmpxchgl %2,(%1)"
+        : "=a" (ret) : "r" (dest), "r" (xchg), "" (compare) : "memory");
+#endif
+    return ret;
+}
+#else
+#define uimon_icep(x, y, z) InterlockedCompareExchangePointer(x, y, z)
+#endif
+
+#if defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__))
+void *uimon_iep(void **dest, void *val)
+{
+    void *ret;
+#ifdef __x86_64__
+    __asm__ volatile ("lock; xchgq %0,(%1)"
+        : "=r" (ret) :"r" (dest), "" (val) : "memory");
+#else
+    __asm__ volatile ("lock; xchgl %0,(%1)"
+        : "=r" (ret) : "r" (dest), "" (val) : "memory" );
+#endif
+    return ret;
+}
+#else
+#define uimon_iep(x, y) InterlockedExchangePointer(x, y)
+#endif
+
 void add_client_window( HWND hwnd )
 {
     uimon_client_windows_t *new_client = lib_malloc( sizeof * new_client );
@@ -330,7 +364,7 @@ static HWND iOpenGeneric(HWND hwnd, DWORD dwStyleMDI, DWORD dwStylePopup, int x,
     window_data->extra->window_type = window_type;
 
     /* wait until we are the only window to be opened */
-    while (InterlockedCompareExchangePointer(&window_data_create, window_data, NULL)) {
+    while (uimon_icep(&window_data_create, window_data, NULL)) {
         ui_dispatch_next_event();
     }
 
@@ -1694,7 +1728,7 @@ static int ExecuteDisassemblyPopup(HWND hwnd, dis_private_t *pdp, LPARAM lParam,
 
     context.xPos = LOWORD(lParam) / pdp->charwidth;
     context.yPos = HIWORD(lParam) / pdp->charheight;
-	context.pdp = pdp;
+        context.pdp = pdp;
 
     return ExecuteGenericPopup(hwnd, lParam, bExecuteDefault, ExecuteDisassemblyPopup_callback, &context);
 }
@@ -2357,7 +2391,7 @@ static LRESULT CALLBACK generic_window_proc(HWND hwnd, UINT msg, WPARAM wParam, 
     window_data.window_procedure = (void *)GetWindowLongPtr(hwnd, offsetof(window_data_t, window_procedure));
 
     if (window_data.window_procedure == NULL) {
-        window_data_t * new_window_data = InterlockedExchangePointer((PULONG) &window_data_create, NULL);
+        window_data_t * new_window_data = uimon_iep((PULONG)&window_data_create, NULL);
 
         assert(new_window_data != NULL);
 

commit 18a926d709b613ad0dbe075c7986322871107b27

Temporarily fixed compilation problem introduced by r22332 (in arch/win32/uimon.c).
git-svn-id: svn+ssh://svn.code.sf.net/p/vice-emu/code/trunk@22384 379a1393-f5fb-40a0-bcee-ef074d9b53f7

vice/src/arch/win32/uimon.c
@@ -163,6 +163,10 @@ static HWND hwndActive = NULL;
 
 static void update_shown(void);
 
+#if 0
+/* This section broke on gcc.exe (GCC) 3.4.2 (mingw-special)
+   commenting it out /tlr */
+
 #if defined(__GNUC__) && (defined(__i386__) || defined(__x86_64__))
 void *uimon_icep(void **dest, void *xchg, void *compare)
 {
@@ -194,6 +198,13 @@ void *uimon_iep(void **dest, void *val)
     return ret;
 }
 #else
+#define uimon_iep(x, y) InterlockedExchangePointer(x, y)
+#endif
+
+#else
+/* Fall back to fix above breakage  /tlr */
+
+#define uimon_icep(x, y, z) InterlockedCompareExchangePointer(x, y, z)
 #define uimon_iep(x, y) InterlockedExchangePointer(x, y)
 #endif
 