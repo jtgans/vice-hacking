jtgans/vice-hacking

commit d9a53fa2da57213f28293f9ed3e4ef3181a5b8a9

Fix quoting of files inside .zip archives, fixes bug #1215
info-zip's unzip needs special quoting of left brackets, since they are wildcards for the unzip command.



git-svn-id: svn+ssh://svn.code.sf.net/p/vice-emu/code/trunk@38054 379a1393-f5fb-40a0-bcee-ef074d9b53f7

vice/src/arch/shared/archdep_quote_unzip.c
@@ -1,0 +1,53 @@
+/** \file   archdep_quote_unzip.c
+ * \brief   Add escape sequences to a filename for use with unzip
+ * \author  Bas Wassink <b.wassink@ziggo.nl>
+ */
+
+/*
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#include "vice.h"
+#include "archdep_defs.h"
+
+#include "lib.h"
+#include "util.h"
+
+#include "archdep_quote_unzip.h"
+
+
+/** \brief  Quote \a name for use as a parameter in exec() etc calls with unzip
+ *
+ * Surounds \a name with double-quotes and replaces brackets with escaped versions.
+ *
+ * \param[in]   name    string to quote
+ *
+ * \return  quoted string
+ */
+char *archdep_quote_unzip(const char *name)
+{
+    char *a;
+    char *b;
+
+    a = util_subst(name, "[", "[[]");
+    b = util_concat("\"", a, "\"", NULL);
+    lib_free(a);
+    return b;
+}

vice/src/arch/shared/Makefile.am
@@ -74,6 +74,7 @@ libarchdep_a_SOURCES = \
 	archdep_program_name.c \
 	archdep_program_path.c \
 	archdep_quote_parameter.c \
+	archdep_quote_unzip.c \
 	archdep_rename.c \
 	archdep_require_vkbd.c \
 	archdep_rmdir.c \
@@ -137,6 +138,7 @@ EXTRA_DIST = \
 	archdep_program_name.h \
 	archdep_program_path.h \
 	archdep_quote_parameter.h \
+	archdep_quote_unzip.h \
 	archdep_rename.h \
 	archdep_require_vkbd.h \
 	archdep_rmdir.h \

vice/src/arch/shared/archdep_quote_unzip.h
@@ -1,0 +1,32 @@
+/** \file   archdep_quote_unzip.h
+ * \brief   Add escape sequences to a filename for use with unzip - header
+ * \author  Bas Wassink <b.wassink@ziggo.nl>
+ */
+
+/*
+ * This file is part of VICE, the Versatile Commodore Emulator.
+ * See README for copyright notice.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+ *  02111-1307  USA.
+ *
+ */
+
+#ifndef VICE_ARCHDEP_QUOTE_UNZIP
+#define VICE_ARCHDEP_QUOTE_UNZIP
+
+char *archdep_quote_unzip(const char *name);
+
+#endif

vice/src/archapi.h
@@ -154,6 +154,7 @@ int         archdep_spawn(const char *name, char **argv,
 /* Spawn need quoting the params or expanding the filename in some archs.  */
 char *      archdep_filename_parameter(const char *name);
 char *      archdep_quote_parameter(const char *name);
+char *      archdep_quote_unzip(const char *name);
 
 /* Allocates a filename and creates a tempfile.  */
 FILE *      archdep_mkstemp_fd(char **filename, const char *mode);

vice/src/zfile.c
@@ -529,7 +529,16 @@ static char *try_uncompress_archive(const char *name, int write_mode,
         argv[5][0] = '3';
         argv[6][0] = '4';
     } else {
-        argv[3] = archdep_quote_parameter(tmp + nameoffset);
+        /* Check for info-zip's unzip
+         *
+         * Unzip needs special quoting of left brackets: [[], not \\[,
+         * see bug #1215.
+         */
+        if (strcmp(program, "unzip") == 0) {
+            argv[3] = archdep_quote_unzip(tmp + nameoffset);
+        } else {
+            argv[3] = archdep_quote_parameter(tmp + nameoffset);
+        }
         argv[4] = NULL;
     }
 

commit 0034ef18df4f9998e841beacec9218b1666f7bd2

Fix unzip breakage on Linux introduced by r38054
Only do the weird unzip quoting on Windows.



git-svn-id: svn+ssh://svn.code.sf.net/p/vice-emu/code/trunk@38055 379a1393-f5fb-40a0-bcee-ef074d9b53f7

vice/src/arch/shared/archdep_quote_unzip.c
@@ -43,6 +43,7 @@
  */
 char *archdep_quote_unzip(const char *name)
 {
+#ifdef ARCHDEP_OS_WINDOWS
     char *a;
     char *b;
 
@@ -50,4 +51,7 @@ char *archdep_quote_unzip(const char *name)
     b = util_concat("\"", a, "\"", NULL);
     lib_free(a);
     return b;
+#else
+    return util_subst(name, "[", "\\[");
+#endif
 }